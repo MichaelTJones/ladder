6d2bd3d8fcf3f48fd4ccffc2a0ff1698de876cb1
MichaelTJones <mtj@google.com>
2014-06-14 08:11:05 -0700

    Update ladder_test.go
    Added two tests

--- ladder_test.go
+++ ladder_test.go
@@ -8,6 +8,130 @@ import "testing"
 // TESTS
 //
 
+// build linear graphs
+//
+//              sum of
+//       pairs   paths   illustration
+//    1:     0       0   O
+//    2:     2       2   O --- O
+//    3:     6       8   O --- O --- O
+//    4:    12      20   O --- O --- O --- O
+//    5:    20      40   O --- O --- O --- O --- O
+//    6:    30      70   O --- O --- O --- O --- O --- O
+//    7:    42     112   O --- O --- O --- O --- O --- O --- O
+//                  :
+//  200: 39800 2666600
+//
+func buildLinearGraph(n int) ([]string, []Indexes, []Component) {
+	node := make([]string, n)
+	// for i := range node {
+	// 	node[i] = fmt.Sprintf("n%d", i)
+	// }
+
+	edge := make([]Indexes, n)
+	for i := range edge {
+		switch {
+		case i == 0:
+			edge[i] = []Index{Index(i + 1)}
+		case i < n-1:
+			edge[i] = []Index{Index(i - 1), Index(i + 1)}
+		default:
+			edge[i] = []Index{Index(i - 1)}
+		}
+	}
+
+	member := make([]Index, n)
+	for i := range member {
+		member[i] = Index(i)
+	}
+	component := []Component{{member, n}}
+
+	return node, edge, component
+}
+
+func TestLinearGraphsV1(t *testing.T) {
+	for n := 1; n <= 200; n++ {
+		pair1 := n * (n - 1)
+		path1 := ((n - 1) * n * (n + 1)) / 3
+
+		node, edge, component := buildLinearGraph(n)
+		pair2, path2 := sumAllSourcesShortestPathsV1(node, edge, component)
+
+		if pair1 != pair2 || path1 != path2 {
+			t.Errorf("%2d: expect (%d, %d) computed (%d, %d)", n, pair1, path1, pair2, path2)
+		}
+	}
+}
+
+func TestLinearGraphsV2(t *testing.T) {
+	for n := 1; n <= 200; n++ {
+		pair1 := n * (n - 1)
+		path1 := ((n - 1) * n * (n + 1)) / 3
+
+		node, edge, component := buildLinearGraph(n)
+		pair2, path2 := sumAllSourcesShortestPathsV2(node, edge, component)
+
+		if pair1 != pair2 || path1 != path2 {
+			t.Errorf("%2d: expect (%d, %d) computed (%d, %d)", n, pair1, path1, pair2, path2)
+		}
+	}
+}
+
+// every node is connected to every other node, all shortest paths have length one
+func buildFullyConnectedGraph(n int) ([]string, []Indexes, []Component) {
+	node := make([]string, n)
+	// for i := range node {
+	// 	node[i] = fmt.Sprintf("n%d", i)
+	// }
+
+	edge := make([]Indexes, n)
+	for i := range edge {
+		edge[i] = make([]Index, n-1)
+		for j := 0; j < i; j++ {
+			edge[i][j] = Index(j) // link to every lower-numbered node
+		}
+		for j := i + 1; j < n; j++ {
+			edge[i][j-1] = Index(j) // link to every higher-numbered node
+		}
+	}
+
+	member := make([]Index, n)
+	for i := range member {
+		member[i] = Index(i)
+	}
+	component := []Component{{member, n}}
+
+	return node, edge, component
+}
+
+func TestFullyConnectedGraphsV1(t *testing.T) {
+	for n := 1; n <= 200; n++ {
+		pair1 := n * (n - 1)
+		path1 := n * (n - 1)
+
+		node, edge, component := buildFullyConnectedGraph(n)
+		pair2, path2 := sumAllSourcesShortestPathsV1(node, edge, component)
+
+		if pair1 != pair2 || path1 != path2 {
+			t.Errorf("%2d: expect (%d, %d) computed (%d, %d)", n, pair1, path1, pair2, path2)
+		}
+	}
+}
+
+func TestFullyConnectedGraphsV2(t *testing.T) {
+	for n := 1; n <= 200; n++ {
+		pair1 := n * (n - 1)
+		path1 := n * (n - 1)
+
+		node, edge, component := buildFullyConnectedGraph(n)
+		pair2, path2 := sumAllSourcesShortestPathsV2(node, edge, component)
+
+		if pair1 != pair2 || path1 != path2 {
+			t.Errorf("%2d: expect (%d, %d) computed (%d, %d)", n, pair1, path1, pair2, path2)
+		}
+	}
+}
+
 //
 // BENCHMARKS
 //
@@ -80,7 +204,7 @@ func BenchmarkFindComponents_webster7(b *testing.B) { benchmarkFindComponents(b,
 func BenchmarkFindComponents_webster8(b *testing.B) { benchmarkFindComponents(b, "words/webster-8", 8) }
 func BenchmarkFindComponents_webster9(b *testing.B) { benchmarkFindComponents(b, "words/webster-9", 9) }
 
-func benchmarkSumASSP(b *testing.B, f string, length int) {
+func benchmarkSumASSPV1(b *testing.B, f string, length int) {
 	word := readWords([]string{f}, length)
 	if len(word) < 1 {
 		b.Logf("no words in input file")
@@ -90,16 +214,64 @@ func benchmarkSumASSP(b *testing.B, f string, length int) {
 	b.ResetTimer()
 
 	for BN := 0; BN < b.N; BN++ {
-		sumAllSourcesShortestPaths(word, pair, component)
+		sumAllSourcesShortestPathsV1(word, pair, component)
 	}
 }
 
-func BenchmarkSumASSP_webster1(b *testing.B) { benchmarkSumASSP(b, "words/webster-1", 1) }
-func BenchmarkSumASSP_webster2(b *testing.B) { benchmarkSumASSP(b, "words/webster-2", 2) }
-func BenchmarkSumASSP_webster3(b *testing.B) { benchmarkSumASSP(b, "words/webster-3", 3) }
-func BenchmarkSumASSP_webster4(b *testing.B) { benchmarkSumASSP(b, "words/webster-4", 4) }
-func BenchmarkSumASSP_webster5(b *testing.B) { benchmarkSumASSP(b, "words/webster-5", 5) }
-func BenchmarkSumASSP_webster6(b *testing.B) { benchmarkSumASSP(b, "words/webster-6", 6) }
-func BenchmarkSumASSP_webster7(b *testing.B) { benchmarkSumASSP(b, "words/webster-7", 7) }
-func BenchmarkSumASSP_webster8(b *testing.B) { benchmarkSumASSP(b, "words/webster-8", 8) }
-func BenchmarkSumASSP_webster9(b *testing.B) { benchmarkSumASSP(b, "words/webster-9", 9) }
+func BenchmarkSumASSPV1_webster1(b *testing.B) { benchmarkSumASSPV1(b, "words/webster-1", 1) }
+func BenchmarkSumASSPV1_webster2(b *testing.B) { benchmarkSumASSPV1(b, "words/webster-2", 2) }
+func BenchmarkSumASSPV1_webster3(b *testing.B) { benchmarkSumASSPV1(b, "words/webster-3", 3) }
+func BenchmarkSumASSPV1_webster4(b *testing.B) { benchmarkSumASSPV1(b, "words/webster-4", 4) }
+func BenchmarkSumASSPV1_webster5(b *testing.B) { benchmarkSumASSPV1(b, "words/webster-5", 5) }
+func BenchmarkSumASSPV1_webster6(b *testing.B) { benchmarkSumASSPV1(b, "words/webster-6", 6) }
+func BenchmarkSumASSPV1_webster7(b *testing.B) { benchmarkSumASSPV1(b, "words/webster-7", 7) }
+func BenchmarkSumASSPV1_webster8(b *testing.B) { benchmarkSumASSPV1(b, "words/webster-8", 8) }
+func BenchmarkSumASSPV1_webster9(b *testing.B) { benchmarkSumASSPV1(b, "words/webster-9", 9) }
+
+// func BenchmarkSumASSPV1_webster10(b *testing.B) { benchmarkSumASSPV1(b, "words/webster-10", 10) }
+// func BenchmarkSumASSPV1_webster11(b *testing.B) { benchmarkSumASSPV1(b, "words/webster-11", 11) }
+// func BenchmarkSumASSPV1_webster12(b *testing.B) { benchmarkSumASSPV1(b, "words/webster-12", 12) }
+// func BenchmarkSumASSPV1_webster13(b *testing.B) { benchmarkSumASSPV1(b, "words/webster-13", 13) }
+// func BenchmarkSumASSPV1_webster14(b *testing.B) { benchmarkSumASSPV1(b, "words/webster-14", 14) }
+// func BenchmarkSumASSPV1_webster15(b *testing.B) { benchmarkSumASSPV1(b, "words/webster-15", 15) }
+// func BenchmarkSumASSPV1_webster16(b *testing.B) { benchmarkSumASSPV1(b, "words/webster-16", 16) }
+// func BenchmarkSumASSPV1_webster17(b *testing.B) { benchmarkSumASSPV1(b, "words/webster-17", 17) }
+// func BenchmarkSumASSPV1_webster18(b *testing.B) { benchmarkSumASSPV1(b, "words/webster-18", 18) }
+// func BenchmarkSumASSPV1_webster19(b *testing.B) { benchmarkSumASSPV1(b, "words/webster-19", 19) }
+// func BenchmarkSumASSPV1_webster20(b *testing.B) { benchmarkSumASSPV1(b, "words/webster-20", 20) }
+
+func benchmarkSumASSPV2(b *testing.B, f string, length int) {
+	word := readWords([]string{f}, length)
+	if len(word) < 1 {
+		b.Logf("no words in input file")
+	}
+	pair := findPairs(word)
+	component := findComponents(word, pair)
+	b.ResetTimer()
+
+	for BN := 0; BN < b.N; BN++ {
+		sumAllSourcesShortestPathsV2(word, pair, component)
+	}
+}
+
+func BenchmarkSumASSPV2_webster1(b *testing.B) { benchmarkSumASSPV2(b, "words/webster-1", 1) }
+func BenchmarkSumASSPV2_webster2(b *testing.B) { benchmarkSumASSPV2(b, "words/webster-2", 2) }
+func BenchmarkSumASSPV2_webster3(b *testing.B) { benchmarkSumASSPV2(b, "words/webster-3", 3) }
+func BenchmarkSumASSPV2_webster4(b *testing.B) { benchmarkSumASSPV2(b, "words/webster-4", 4) }
+func BenchmarkSumASSPV2_webster5(b *testing.B) { benchmarkSumASSPV2(b, "words/webster-5", 5) }
+func BenchmarkSumASSPV2_webster6(b *testing.B) { benchmarkSumASSPV2(b, "words/webster-6", 6) }
+func BenchmarkSumASSPV2_webster7(b *testing.B) { benchmarkSumASSPV2(b, "words/webster-7", 7) }
+func BenchmarkSumASSPV2_webster8(b *testing.B) { benchmarkSumASSPV2(b, "words/webster-8", 8) }
+func BenchmarkSumASSPV2_webster9(b *testing.B) { benchmarkSumASSPV2(b, "words/webster-9", 9) }
+
+// func BenchmarkSumASSPV2_webster10(b *testing.B) { benchmarkSumASSPV2(b, "words/webster-10", 10) }
+// func BenchmarkSumASSPV2_webster11(b *testing.B) { benchmarkSumASSPV2(b, "words/webster-11", 11) }
+// func BenchmarkSumASSPV2_webster12(b *testing.B) { benchmarkSumASSPV2(b, "words/webster-12", 12) }
+// func BenchmarkSumASSPV2_webster13(b *testing.B) { benchmarkSumASSPV2(b, "words/webster-13", 13) }
+// func BenchmarkSumASSPV2_webster14(b *testing.B) { benchmarkSumASSPV2(b, "words/webster-14", 14) }
+// func BenchmarkSumASSPV2_webster15(b *testing.B) { benchmarkSumASSPV2(b, "words/webster-15", 15) }
+// func BenchmarkSumASSPV2_webster16(b *testing.B) { benchmarkSumASSPV2(b, "words/webster-16", 16) }
+// func BenchmarkSumASSPV2_webster17(b *testing.B) { benchmarkSumASSPV2(b, "words/webster-17", 17) }
+// func BenchmarkSumASSPV2_webster18(b *testing.B) { benchmarkSumASSPV2(b, "words/webster-18", 18) }
+// func BenchmarkSumASSPV2_webster19(b *testing.B) { benchmarkSumASSPV2(b, "words/webster-19", 19) }
+// func BenchmarkSumASSPV2_webster20(b *testing.B) { benchmarkSumASSPV2(b, "words/webster-20", 20) }
